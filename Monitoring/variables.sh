eb2871e4558a349e0fed09154601516f0fc30ebd
Egor Petrov <iampetroved@gmail.com>
2022-11-06 23:21:59 +0400

    Add files via upload

---
+++ Monitoring/variables.sh
@@ -0,0 +1,4 @@
+#DeFund monitoring variables template 
+COS_BIN_NAME=insert_path_to_archway_binary # example: /root/go/bin/defundd or /home/user/go/bin/defundd
+COS_PORT_RPC=insert_port_here              # default: 26657
+COS_VALOPER=insert_valoper_address         # example: defundvaloper1234545636767376535673
